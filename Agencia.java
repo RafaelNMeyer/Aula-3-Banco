7c6fe40275e8437ffd843f513db174fb0adabecd
tobizen <48719850+tobizen@users.noreply.github.com>
2019-03-19 16:41:26 -0300

    Update Agencia.java

--- Agencia.java
+++ Agencia.java
@@ -1,5 +1,3 @@
-package kmnbblj;
-
 import java.util.ArrayList;
 
 public class Agencia {
@@ -9,5 +7,23 @@ public class Agencia {
 	
 	 ArrayList<Conta> contas = new ArrayList<Conta>();
 	ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 	
 }
